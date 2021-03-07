1294337422a850e097ae08b1431aa60b4c7074bc
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2021-03-07 13:56:12 -0300

    add Forgotten

---
+++ src/main/java/com/vinibrenobr11/springhibernatedemo/apis/PersonApi.java
@@ -0,0 +1,31 @@
+package com.vinibrenobr11.springhibernatedemo.apis;
+
+import com.vinibrenobr11.springhibernatedemo.models.Person;
+import com.vinibrenobr11.springhibernatedemo.repository.PersonRepository;
+import com.vinibrenobr11.springhibernatedemo.repository.interfaces.PersonRepositoryInterface;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/person", produces = { MediaType.APPLICATION_JSON_VALUE })
+public class PersonApi {
+
+    @Autowired
+    private PersonRepositoryInterface personRepo;
+    
+    @GetMapping
+    public Iterable<Person> getAll() {
+        return personRepo.findAll();
+    }
+
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public Person persist(Person p) {
+        return personRepo.save(p);
+    }
+}
+
